f2032f90f76efac19e1e1f9cd6d651be2639579c
Julien <julien.baillagou@gmail.com>
2021-01-28 20:39:01 +0100

    Add docker-compose.yml to bootstrap components

--- scripts/mongo/01-Create-All-MongoDb-Collections.js
+++ scripts/mongo/01-Create-All-MongoDb-Collections.js
@@ -1,19 +1,28 @@
 const collections = [
+	"agreements",
 	"announcements",
+	"bookmarks",
 	"component-categories",
 	"components",
 	"disclaimers",
 	"incidents",
 	"incidents-components",
 	"incidents-history",
-        "links",
+    "links",
 	"planned-maintenances",
 	"planned-maintenances-history",
-	"planned-maintenances-components"
+	"planned-maintenances-components",
+	"users",
+	"users-authentications",
+	"users-mails",
+	"users-mobile-phones",
+	"users-preferences",
+	"users-profile",
+	"users-sessions"
 ];
 
 collections.forEach((collection) => {
-        print("> Dropping/Creating collection: " + collection);
+    print("> Dropping/Creating collection: " + collection);
 	db.getCollection(collection).drop();
 	db.createCollection(collection);
 });
